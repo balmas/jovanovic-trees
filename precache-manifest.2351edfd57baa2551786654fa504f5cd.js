b476cd2952e80843749156a6efff796d12d23c1a
nevenjovanovic <neven.jovanovic@ffzg.hr>
2020-06-26 15:35:43 +0000

    Deploying to gh-pages from  @ f54ad0c00ac28374827bcb088986e3f1b864741c 🚀

---
+++ precache-manifest.2351edfd57baa2551786654fa504f5cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a924735da9afdcfddf3e767822661dce",
+    "url": "/treebank-template/index.html"
+  },
+  {
+    "revision": "2dcbad217f7877ce0091",
+    "url": "/treebank-template/static/css/2.354f031a.chunk.css"
+  },
+  {
+    "revision": "06c86848939703336a89",
+    "url": "/treebank-template/static/css/main.bd98509e.chunk.css"
+  },
+  {
+    "revision": "2dcbad217f7877ce0091",
+    "url": "/treebank-template/static/js/2.1716a128.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/treebank-template/static/js/2.1716a128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c86848939703336a89",
+    "url": "/treebank-template/static/js/main.feb8edd4.chunk.js"
+  },
+  {
+    "revision": "fc82e1a45c5889b39b98",
+    "url": "/treebank-template/static/js/runtime-main.94c54c7b.js"
+  }
+]);
